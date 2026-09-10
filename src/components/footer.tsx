cf6005f6dad74541424426bb86e8bdcf7e1feb5f
Yuri Alves <yuri.alves.silva@gmail.com>
2026-09-10 18:01:37 -0300

    feat: adiciona imagem fatec no footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,22 +3,25 @@ import Image from "next/image";
 export function Footer() {
   return (
     <footer className="mt-10 bg-gradient-to-r from-[#17264D] to-[#004A53] px-5 py-10 text-white">
-      <div className="mx-auto flex max-w-6xl flex-col gap-8 lg:flex-row lg:items-center">
-        <div className="flex items-center gap-5 border-white/50 lg:w-[300px] lg:border-r lg:pr-7">
+      <div className="mx-auto flex max-w-7xl flex-col gap-8 lg:flex-row lg:items-center">
+        <div className="flex items-center gap-5 lg:w-[320px]">
           <Image
             src="/images/logo_fatec_br.png"
             alt="Logo da Fatec"
-            width={50}
-            height={50}
-            className="h-auto w-12"
-           />
-          <Image
-            src="/images/administrator/institutional-logos.png"
-            alt="Fatec, Centro Paula Souza e Governo do Estado de São Paulo"
             width={2170}
             height={621}
-            className="h-auto w-72"
+            className="h-12 mt-3 w-auto object-contain"
           />
+
+          <div className="border-r border-white/50 pr-7">
+            <Image
+              src="/images/administrator/institutional-logos.png"
+              alt="Fatec, Centro Paula Souza e Governo do Estado de São Paulo"
+              width={2170}
+              height={621}
+              className="w-55 object-contain"
+            />
+          </div>
         </div>
 
         <div className="grid flex-1 grid-cols-2 gap-7 sm:grid-cols-5">
